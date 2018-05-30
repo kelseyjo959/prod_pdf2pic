4dfd03ff7afe2c8f16913a29a3eb41b6ce0441d6
Simon Raden <simon.raden@graybar.com>
2018-05-30 10:25:17 -0500

    getting a database connection

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var db = require('./db');
+var db = require('./db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,17 +23,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// // Connect to Mongo on start
-// db.connect('mongodb://localhost:27017/mydatabase', function(err) {
-//   if (err) {
-//     console.log('Unable to connect to Mongo.')
-//     process.exit(1)
-//   } else {
-//     app.listen(3000, function() {
-//       console.log('Listening on port 3000...')
-//     })
-//   }
-// })
+// Connect to Mongo on start
+db.connect('mongodb://localhost:27017/mydatabase', function(err) {
+  if (err) {
+    console.log('Unable to connect to Mongo.')
+    process.exit(1)
+  } else {
+    app.listen(8000, function() {
+      console.log('Database listening on port 8000...')
+    })
+  }
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
